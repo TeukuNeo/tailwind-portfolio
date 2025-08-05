e2259ebdfa20ac1db95b55bbba3c5837ff1f54bc
TeukuNeo <teukuneoquinn@gmail.com>
2025-08-05 15:34:19 +0700

    Mengaktifkan fitur send message to whatsapp

--- TAILWIND PORTFOLIO/js/script.js
+++ TAILWIND PORTFOLIO/js/script.js
@@ -4,7 +4,7 @@ window.onscroll = function() {
     const fixedNav = header.offsetTop
 
     if (window.scrollY > fixedNav) {
-       header.classList.add('navbar-fixed') 
+       header.classList.add('navbar-fixed')
     } else {
         header.classList.remove('navbar-fixed')
     }
@@ -20,3 +20,19 @@ hamburger.addEventListener('click', function() {
     navMenu.classList.toggle('hidden')
 })
 
+// send to whatsapp
+function sendToWhatsApp(){
+    let number = "+6282362090168"
+
+    let name = document.getElementById('name').value
+    let email = document.getElementById('email').value
+    let message = document.getElementById('message').value
+
+    let url = "https://wa.me/" + number + "?text="
+    + "Name : " +name+ "%0a"
+    + "Email : " +email+ "%0a"
+    + "Message : " +message+ "%0a%0a"
+
+    window.open(url, '_blank').focus()
+
+}
